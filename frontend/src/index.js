ccfa223d84422be342f716dff0252c16cb926dac
BukenyaLukman <bukenyalukman@gmail.com>
2020-07-08 15:03:31 +0300

    Django Chat end and React FrontEnd

---
+++ frontend/src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+               <div>Hello</div> 
+            </div>
+        )
+    }
+}
+ReactDOM.render(<App/>, document.getElementById('app'));
+
+// export default App
